74ceeff999ef95c9f1a37ea4fef6ad5533dcbb34
Celeste A. Seberras <38491560+har00ga@users.noreply.github.com>
2020-07-02 13:21:58 -0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -90,7 +90,6 @@ module.exports = {
                 sidebarDepth: 1,
                 collapsable: false,
                 children: [
-                  '/how-to/mining',
                   '/how-to/store/prepare-data',
                   '/how-to/store/tokens',
                   '/how-to/store/making-storage-deals',
