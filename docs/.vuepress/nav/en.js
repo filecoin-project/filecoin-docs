c600e5a0eb138680b7761e5ee2560ad6fe425b2e
Chris Waring <chris@wwaves.co>
2020-08-11 20:44:39 +0100

    layout primary nav

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -2,5 +2,19 @@ module.exports = [
   { text: 'Get Started', link: '/' },
   { text: 'Mine', link: '/mine/' },
   { text: 'Build', link: '/build/' },
-  { text: 'Reference', link: '/reference/' }
+  { text: 'Reference', link: '/reference/' },
+  {
+    text: 'Get Involved',
+    ariaLabel: 'Get Involved',
+    items: [
+      {
+        text: 'Project',
+        link: '/project/'
+      },
+      {
+        text: 'Community',
+        link: '/community/'
+      }
+    ]
+  }
 ]
