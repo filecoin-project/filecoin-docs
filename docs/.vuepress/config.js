add8b056f61d9d8bdf39e98b7762455a70c808a9
Celeste A. Seberras <38491560+har00ga@users.noreply.github.com>
2020-08-18 21:14:59 -0400

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,7 +21,7 @@ module.exports = {
       md.use(require('markdown-it-footnote'))
       md.use(require('markdown-it-task-lists'))
       md.use(require('markdown-it-deflist'))
-    }
+    } 
   },
   themeConfig: {
     algolia: {
