a15807a4a7c7fcdd5e7b276972700b86b765e27c
johnnymatthews <johnnymatthews@protonmail.com>
2021-06-28 20:57:37 -0400

    Moves nearly everything around.

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,6 +1,7 @@
 module.exports = [
   { text: 'About Filecoin', link: '/' },
   { text: 'Get Started', link: '/get-started/' },
+  { text: 'Lotus', link: '/lotus/' },
   { text: 'Store', link: '/store/' },
   { text: 'Mine', link: '/mine/' },
   { text: 'Build', link: '/build/' },
