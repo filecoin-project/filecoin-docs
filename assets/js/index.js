62a6cb7ed5e87e5851bd2ca390b6021e85b8fc04
johnnymatthews <johnnymatthews@protonmail.com>
2022-02-24 12:37:02 -0500

    Search bar works sitewide.

--- assets/js/index.js
+++ assets/js/index.js
@@ -95,7 +95,7 @@ Source:
 
   // https://discourse.gohugo.io/t/range-length-or-last-element/3803/2
 
-  {{ $list := (where .Site.Pages "Section" "docs") -}}
+  {{ $list := where site.RegularPages "Type" "in" site.Params.mainSections -}}
   {{ $len := (len $list) -}}
 
   index.add(
