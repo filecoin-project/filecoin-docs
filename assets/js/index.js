50f3aa6dd4722003892d1a9f849d10efd4cf31a6
johnnymatthews <johnnymatthews@protonmail.com>
2022-04-01 16:32:20 -0400

    Adds 'about-filecoin' section.

--- assets/js/index.js
+++ assets/js/index.js
@@ -94,7 +94,7 @@ Source:
 
   // https://discourse.gohugo.io/t/range-length-or-last-element/3803/2
 
-  {{ $list := (where .Site.Pages "Section" "docs") -}}
+  {{ $list := (.Site.Pages) -}}
   {{ $len := (len $list) -}}
 
   index.add(
