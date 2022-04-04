79ddd014f238811d444e6176f9bf2eb5a2dddc82
johnnymatthews <johnnymatthews@protonmail.com>
2022-04-04 12:59:12 -0400

    Adds shell and plaintext syntax.

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,21 +1,22 @@
 import hljs from 'highlight.js/lib/core';
 
+import ini from 'highlight.js/lib/languages/ini';
 import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
-import bash from 'highlight.js/lib/languages/bash';
-import xml from 'highlight.js/lib/languages/xml';
-import ini from 'highlight.js/lib/languages/ini';
-import yaml from 'highlight.js/lib/languages/yaml';
 import markdown from 'highlight.js/lib/languages/markdown';
+import plaintext from 'highlight.js/lib/languages/plaintext';
+import shell from 'highlight.js/lib/languages/shell';
+import yaml from 'highlight.js/lib/languages/yaml';
+import xml from 'highlight.js/lib/languages/xml';
 
+hljs.registerLanguage('ini', ini);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
-hljs.registerLanguage('bash', bash);
-hljs.registerLanguage('html', xml);
-hljs.registerLanguage('ini', ini);
-hljs.registerLanguage('toml', ini);
-hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('md', markdown);
+hljs.registerLanguage('plaintext', plaintext);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('html', xml);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
